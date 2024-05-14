2dc6862df29b96a99605b5ac3ea3b2c97719cc59
Oscar Urrego <osureg@gmail.com>
2024-05-14 18:23:26 -0500

    Primeras Practicas JS

---
+++ JavaScript.js
@@ -0,0 +1,27 @@
+console.log("Hello world!");
+
+console.log("Hola Mundo")
+console.log("Aqui es mi primer programa en JavaScript")
+console.log(12334)
+
+// Tipos de datos
+
+/* Let declara una variable modificable */
+
+let nombre = "oscar"
+let edad = 18
+
+console.log(nombre)
+
+let n1 = 7
+let n2 = 6
+
+console.log(n1)
+console.log(n2)
+console.log(n1 + n2)
+
+
+/* Declarar una constante la cual su valor no podra modificarse*/
+
+const pi = 3.1416
+console.log(pi)
